ee9cb7d3e792d4aaabcc7d3393a2990ea2c97205
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:01:14 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,22 +6,22 @@ from conans import ConanFile, tools
 
 class BoostConvertConan(ConanFile):
     name = "boost_convert"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_convert"
     
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_core/1.66.0@bincrafters/testing",
-        "boost_function_types/1.66.0@bincrafters/testing",
-        "boost_lexical_cast/1.66.0@bincrafters/testing",
-        "boost_math/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_optional/1.66.0@bincrafters/testing",
-        "boost_parameter/1.66.0@bincrafters/testing",
-        "boost_range/1.66.0@bincrafters/testing",
-        "boost_spirit/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing",
+        "boost_config/1.65.1@bincrafters/testing",
+        "boost_core/1.65.1@bincrafters/testing",
+        "boost_function_types/1.65.1@bincrafters/testing",
+        "boost_lexical_cast/1.65.1@bincrafters/testing",
+        "boost_math/1.65.1@bincrafters/testing",
+        "boost_mpl/1.65.1@bincrafters/testing",
+        "boost_optional/1.65.1@bincrafters/testing",
+        "boost_parameter/1.65.1@bincrafters/testing",
+        "boost_range/1.65.1@bincrafters/testing",
+        "boost_spirit/1.65.1@bincrafters/testing",
+        "boost_type_traits/1.65.1@bincrafters/testing"
     )
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -36,7 +36,7 @@ class BoostConvertConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
